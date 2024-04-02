7e41d57940a38c874761bb98c30029f1ce26267f
Gabriel <gabrielhenriquedps@gmail.com>
2024-04-02 12:49:11 -0300

    first

---
+++ src/test/java/br/com/gabrielhenrique/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielhenrique.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
