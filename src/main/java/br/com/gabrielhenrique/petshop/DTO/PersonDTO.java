cabb16d12f497a9af567fdd52600c861497afb52
Gabriel <gabrielhenriquedps@gmail.com>
2024-04-02 13:13:45 -0300

    feat: add PersonDto

---
+++ src/main/java/br/com/gabrielhenrique/petshop/DTO/PersonDTO.java
@@ -0,0 +1,11 @@
+package br.com.gabrielhenrique.petshop.DTO;
+
+import lombok.Data;
+
+@Data
+public class PersonDTO {
+    private String name;
+    private String surname;
+    private String phone;
+    private String identifier;
+}
